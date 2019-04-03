6f023373db4b220adba887a0147b13eb4cf649d2
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2019-04-03 08:27:08 -0500

    primer commit, servidor inicial funcionando

---
+++ index.js
@@ -0,0 +1,33 @@
+// importar el módulo express
+var express = require('express');
+
+// crear la variable app usando express
+var app = express();
+
+// configurar la carpeta public como "pública"
+app.use(express.static('public'));
+
+// configurar la ruta inicial
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/home.html');
+});
+
+// configurar la ruta contacto
+app.get('/contacto', function(request, response){
+    response.send('página contacto');
+});
+
+// configurar la ruta sobre-mi
+app.get('/sobre-mi', function(request, response){
+    response.send('página sobre mi');
+});
+
+// configurar la ruta portafolio
+app.get('/portafolio', function(request, response){
+    response.send('mi portafolio');
+});
+
+// iniciar el servidor en el puerto 3000
+app.listen(3000, function() {
+  console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
