e4a54bfb78f7822c6a0edb3b41a6bc186f5fedb4
Sebastián Gaviria <vagyriag@hotmail.com>
2019-04-05 08:57:56 -0500

    productos en arreglo y ruta dinámica

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ var contador = {
     contacto: 0,
 };
 
-
+var productos = [];
+productos.push({
+    titulo: 'Perro',
+    precio: '85714312312',
+    imagen: 'https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/5c3871215bafe83b078adbe3/perro.jpg',
+    descripcion: 'Si te has decidido a tener un perro, seguro que antes de comprar –o mejor, adoptar- a tu nueva mascota, tendrás qué decidir qué raza prefieres en función de tu modo de vida y tu propia personalidad. Hay perros de carácter más agresivo o dominante, otros mansos y alegres, algunos necesitan mucho espacio para correr y jugar, otros son más tranquilos…'
+});
+productos.push({
+    titulo: 'Gato',
+    precio: '8571431231200000',
+    imagen: 'https://www.feelcats.com/blog/wp-content/uploads/2018/10/gato-atigrado.jpg',
+    descripcion: 'Esa preciosa combinación de patrones jaspeados, moteados o rayados en diferentes tonos, nos da a la idea de una genética caprichosa, que te contaremos, en este original artículo sobre ellos, pero además nos adentraremos en su carácter y peculiaridades, de estos gatos atigrados tan comunes, pero a la vez, tan únicos. ¿empezamos?'
+});
 
 // configurar la ruta inicial
 app.get('/', function(req, res) {
@@ -36,23 +48,9 @@ app.get('/tienda', function(req, res) {
     res.sendFile(__dirname + '/public/home.html');
 });
 
-app.get('/tienda/perro', function(req, res) {
-    var contexto = {
-        titulo: 'Perro',
-        precio: '85714312312',
-        imagen: 'https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/5c3871215bafe83b078adbe3/perro.jpg',
-        descripcion: 'Si te has decidido a tener un perro, seguro que antes de comprar –o mejor, adoptar- a tu nueva mascota, tendrás qué decidir qué raza prefieres en función de tu modo de vida y tu propia personalidad. Hay perros de carácter más agresivo o dominante, otros mansos y alegres, algunos necesitan mucho espacio para correr y jugar, otros son más tranquilos…'
-    };
-    res.render('producto', contexto);
-});
-
-app.get('/tienda/gato', function(req, res) {
-    var contexto = {
-        titulo: 'Gato',
-        precio: '8571431231200000',
-        imagen: 'https://www.feelcats.com/blog/wp-content/uploads/2018/10/gato-atigrado.jpg',
-        descripcion: 'Esa preciosa combinación de patrones jaspeados, moteados o rayados en diferentes tonos, nos da a la idea de una genética caprichosa, que te contaremos, en este original artículo sobre ellos, pero además nos adentraremos en su carácter y peculiaridades, de estos gatos atigrados tan comunes, pero a la vez, tan únicos. ¿empezamos?'
-    };
+app.get('/tienda/:producto', function(req, res) {
+    var contexto = productos[0];
+    console.log(req.params.producto);
     res.render('producto', contexto);
 });
 
